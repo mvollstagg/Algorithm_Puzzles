3732e963c06d6b30a9af6c3654316c71371027c6
mvollstagg <mvollstagg@gmail.com>
2022-07-19 00:10:15 +0300

    Day 12, 13, 14

--- Program.cs
+++ Program.cs
@@ -173,6 +173,146 @@
 // }
 // Console.WriteLine(hourglass);
 
+// Day 12: Inheritance
+// class Person{
+// 	protected string firstName;
+// 	protected string lastName;
+// 	protected int id;
+	
+// 	public Person(){}
+// 	public Person(string firstName, string lastName, int identification){
+// 			this.firstName = firstName;
+// 			this.lastName = lastName;
+// 			this.id = identification;
+// 	}
+// 	public void printPerson(){
+// 		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+// 	}
+// }
+// class Student : Person{
+//     private int[] testScores;  
+  
+//     /*	
+//     *   Class Constructor
+//     *   
+//     *   Parameters: 
+//     *   firstName - A string denoting the Person's first name.
+//     *   lastName - A string denoting the Person's last name.
+//     *   id - An integer denoting the Person's ID number.
+//     *   scores - An array of integers denoting the Person's test scores.
+//     */
+//     // Write your constructor here
+//     public Student(string firstName, string lastName, int identification, int[] testScores)
+//     {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.id = identification;
+//         this.testScores = testScores;
+//     }
+    
+//     /*	
+//     *   Method Name: Calculate
+//     *   Return: A character denoting the grade.
+//     */
+//     public char Calculate()
+//     {
+//         var score = this.testScores.Sum() / this.testScores.Count();
+//         if(score >= 90 && score <= 100)
+//             return 'O';
+//         else if(score >= 80 && score < 90)
+//             return 'E';
+//         else if(score >= 70 && score < 80)
+//             return 'A';
+//         else if(score >= 55 && score < 70)
+//             return 'P';
+//         else if(score >= 40 && score < 55)
+//             return 'D';
+//         else
+//             return 'T';
+//     }
+// }
+
+// string[] inputs = Console.ReadLine().Split();
+// string firstName = inputs[0];
+// string lastName = inputs[1];
+// int id = Convert.ToInt32(inputs[2]);
+// int numScores = Convert.ToInt32(Console.ReadLine());
+// inputs = Console.ReadLine().Split();
+// int[] scores = new int[numScores];
+// for(int i = 0; i < numScores; i++){
+//     scores[i]= Convert.ToInt32(inputs[i]);
+// } 
+
+// Student s = new Student(firstName, lastName, id, scores);
+// s.printPerson();
+// Console.WriteLine("Grade: " + s.Calculate() + "\n");
+
+// Day 13: Abstract Classes
+// abstract class Book
+// {
+    
+//     protected String title;
+//     protected  String author;
+    
+//     public Book(String t,String a){
+//         title=t;
+//         author=a;
+//     }
+//     public abstract void display();
+
+
+// }
+
+// //Write MyBook class
+// class MyBook : Book
+// {
+//     private int price = 0;
+    
+//     public MyBook(string title, string author, int price) : base(title, author)
+//     {
+//         this.price = price;
+//     }
+    
+//     public override void display()
+//     {
+//         Console.WriteLine("Title: " + this.title);
+//         Console.WriteLine("Author: " + this.author);
+//         Console.WriteLine("Price: " + this.price);
+//     }
+// }
+
+// Day 14: Scope
+// Difference d = new Difference(new int[] { 8, 19, 3, 2, 7 });
+// d.computeDifference();
+// Console.Write(d.maximumDifference);
+
+// class Difference {
+//     private int[] elements;
+//     public int maximumDifference;
+
+// 	// Add your code here
+//     public Difference(int[] elements)
+//     {
+//         this.elements = elements;
+//     }
+    
+//     public void computeDifference()
+//     {
+//         while(elements.Count() > 1)
+//         {
+//             for(int i = 0; i < elements.Length; i++)
+//             {
+//                 maximumDifference = Math.Max(Math.Abs(elements.FirstOrDefault() - elements[i]), maximumDifference);                
+//             }
+//             elements = elements.Where(x => x != elements.FirstOrDefault()).ToArray();
+//         }
+//     }
+// }
+
+// Day 15: Linked List
+        
+
+
 
 // ************ 1 Week Preparation Kit ************
 // Day 1 - Plus Minus
