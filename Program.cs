d2c73cd231bd653036ab46501cc8a810ec32698b
mvollstagg <mvollstagg@gmail.com>
2022-07-29 22:00:02 +0300

    1 Week Preparation Day 1, 2

--- Program.cs
+++ Program.cs
@@ -878,6 +878,8 @@
 //     public string Company { get; set; }
 // }
 
+// ************ C# Basic Test End ************
+
 
 // ************ 1 Week Preparation Kit ************
 // Day 1 - Plus Minus
@@ -906,23 +908,87 @@
 // Day 1 - Time Conversion
 // static string timeConversion(string s)
 // {
-//     List<string> stringList = s.Split(':').ToList();
-//     int hour = stringList[2].Contains("PM") && Convert.ToInt16(stringList[0]) + 12 != 24 ? (Convert.ToInt16(stringList[0]) == 12 ? 0 : Convert.ToInt16(stringList[0]) + 12) : (Convert.ToInt16(stringList[0]) == 12 ? 0 : Convert.ToInt16(stringList[0]) );
-//     if(stringList[2].Contains("PM") && Convert.ToInt16(stringList[0]) == 12) // !* This controll will be add up later
-//     {
-//         hour = 12;
-//     }
-//     int minute = Convert.ToInt16(stringList[1]);
-//     int seconds = Convert.ToInt16(stringList[2].Remove(stringList[2].Length - 2));
-//     return hour.ToString("D2") + ":" + minute.ToString("D2") + ":" + seconds.ToString("D2");
+//     DateTime  date = DateTime.Parse(s);
+//     return date.ToString("HH:mm:ss");
 // }
 // Console.WriteLine(timeConversion("12:45:54PM"));
 
 // Day 1 - Mock Test - Find Median
-// using System.Linq;
-// static int findMedian(List<int> arr)
+// Console.WriteLine(Result.findMedian(new List<int> { 0, 1, 2, 3, 4, 5, 6 }));
+// class Result
 // {
-//     return arr.OrderBy(x => x).Skip((arr.Count() - 1) / 2).First();
+//     public static int findMedian(List<int> arr)
+//     {
+//         var count = arr.Count();
+//         if(count % 2 == 0)
+//         {
+//             return arr[count / 2] + arr[count / 2 - 1] / 2;
+//         }
+//         else
+//         {
+//             return arr[count / 2];
+//         }
+//     }
 // }
-// Console.WriteLine(findMedian(new List<int> { 0, 1, 2, 3, 4, 5, 6 }));
 
+// Day 2 - Lonely Integer
+// Console.WriteLine(Result.lonelyinteger(new List<int> { 1, 2, 3, 4, 3, 2, 1 }));
+// class Result
+// {
+//     public static int lonelyinteger(List<int> a)
+//     {
+//         return a.Distinct().First(x => a.Count(y => y == x) == 1);
+//     }
+// }
+
+// Day 2 - Diagonal Difference
+// Console.WriteLine(Result.diagonalDifference(new List<List<int>> { new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 } }));
+// class Result
+// {
+//     public static int diagonalDifference(List<List<int>> arr)
+//     {
+//         var count = arr.Count();
+//         int rightDiagonal = 0, leftDiagonal = 0;
+//         for (int i = 0; i < count; i++)
+//         {
+//             rightDiagonal += arr[i][i];
+//             leftDiagonal += arr[i][count - i - 1];
+//         }
+//         return Math.Abs(rightDiagonal - leftDiagonal);
+//     }
+// }
+
+// Day 2 - Counting Sort 1
+// var result = Result.countingSort(new List<int> {    63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99,
+//                                                     12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30,
+//                                                     27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23,
+//                                                     41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33,
+//                                                     60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33 });
+// Console.WriteLine(string.Join(", ", result));
+// class Result
+// {
+//     public static List<int> countingSort(List<int> arr)
+//     {
+//         arr.Sort();
+//         var count = arr.Count();
+//         var result = new int[100];
+//         for (int i = 0; i < count; i++)
+//         {
+//             result[arr[i]]++;
+//         }
+//         return result.ToList();
+//     }
+// }
+
+// Day 2 - Mock Test - Flipping the Matrix
+Console.WriteLine(Result.flippingMatrix(new List<List<int>> {   new List<int> { 112, 42, 83, 119 },
+                                                                new List<int> { 56, 125, 56, 49 },
+                                                                new List<int> { 15, 78, 101, 43 },
+                                                                new List<int> { 62, 98, 114, 108 } }));
+class Result
+{
+    public static int flippingMatrix(List<List<int>> matrix)
+    {
+        return matrix.Select(x => x.Sum()).Sum();
+    }
+}
